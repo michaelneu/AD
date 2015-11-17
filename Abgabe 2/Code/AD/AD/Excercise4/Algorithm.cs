0ba10b2ed068c2ffa744bbe62f294a9ea8e8092d
minedev <jytho@web.de>
2015-11-17 17:19:24 +0100

    Adds task 2

---
+++ Abgabe 2/Code/AD/AD/Excercise4/Algorithm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AD.Excercise4
+{
+    public class Algorithm
+    {
+        public static int Recursion(int n, int m)
+        {
+            if (n == 0)
+            {
+                return m + 1;
+            }
+            else if (m == 0 && n >= 1)
+            {
+                return Recursion(n - 1, 1);
+            }
+            else
+            {
+                return Recursion(n - 1, Recursion(n, m - 1));
+            }
+        }
+
+        public static int Iteration(int n, int m)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(n);
+            stack.Push(m);
+
+            while (stack.Count != 0)
+            {
+                m = stack.Pop();
+                n = stack.Pop();
+
+                if (n == 0)
+                {
+                    // "recursion end"
+
+                    if (stack.Count > 0)
+                    {
+                        // coming from previous "inner recursion"
+                        stack.Pop();
+                        n = stack.Pop();
+
+                        stack.Push(n);
+                        stack.Push(m + 1);
+                    }
+                }
+                else if (m == 0 && n >= 1)
+                {
+                    stack.Push(n - 1);
+                    stack.Push(1);
+                }
+                else
+                {
+                    // outer recursion call
+                    stack.Push(n - 1);
+                    stack.Push(m);
+
+                    // inner recursion call
+                    stack.Push(n);
+                    stack.Push(m - 1);
+                }
+            }
+
+            return m + 1;
+        }
+    }
+}
---
+++ Abgabe 2/Code/AD/AD/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using AD.Excercise4;
+
+namespace AD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" m  | n  | recursion | iteration ");
+            string tableRow = " {0,2} | {1,2} | {2,9} | {3,9} ",
+                tableSeparator = "----+----+-----------+-----------";
+
+            for (int m = 0; m < 4; m++)
+            {
+                for (int n = 0; n < 11; n++)
+                {
+                    int recursion = Algorithm.Recursion(m, n),
+                        iteration = Algorithm.Iteration(m, n);
+
+                    if (recursion != iteration)
+                    {
+                        Console.WriteLine(string.Format("\nMismatching results: {0} - {1} ({2},{3})\n", recursion, iteration, m, n));
+                        Environment.Exit(1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(tableSeparator);
+                        Console.WriteLine(string.Format(tableRow, m, n, recursion, iteration));
+                    }
+                }
+            }
+        }
+    }
+}
