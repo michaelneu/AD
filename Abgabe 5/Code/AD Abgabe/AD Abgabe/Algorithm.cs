3437341bb408f322b9d3be69871bb1b23ebe55de
minedev <jytho@web.de>
2015-11-17 17:20:26 +0100

    Adds task 5

---
+++ Abgabe 5/Code/AD Abgabe/AD Abgabe/Algorithm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AD_Abgabe
+{
+    class Algorithm
+    {
+        public static bool ContainsSum(int[] array)
+        {
+            if (array.Length < 2)
+            {
+                return false;
+            }
+
+            List<int> list = array.ToList();
+            int target = list.Last();
+
+            list = list.Take(array.Length - 1).ToList();
+            list.Sort();
+            array = list.ToArray();
+
+            int min = 0,
+                max = array.Length - 1;
+
+            while (min < max)
+            {
+                int sum = array[min] + array[max];
+
+                if (sum == target)
+                {
+                    return true;
+                }
+                else if (sum < target)
+                {
+                    min++;
+                }
+                else
+                {
+                    max--;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Abgabe 5/Code/AD Abgabe/AD Abgabe/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AD_Abgabe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+
+            Console.WriteLine(
+                "[{0}]: {1}",
+                string.Join(", ", numbers.Select(x => x + "").ToArray()),
+                Algorithm.ContainsSum(numbers)
+            );
+
+            numbers[numbers.Length - 1] = numbers.Take(numbers.Length - 1).Sum();
+
+            Console.WriteLine(
+                "[{0}]: {1}",
+                string.Join(", ", numbers.Select(x => x + "").ToArray()),
+                Algorithm.ContainsSum(numbers)
+            );
+        }
+    }
+}
